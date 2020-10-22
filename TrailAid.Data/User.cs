0c43994a6f3bc9c25c3759d144310e60188cb3c4
dfdevaney <dfdevaney@gmail.com>
2020-10-22 10:47:08 -0500

    User Data & Create Update

--- TrailAid.Data/User.cs
+++ TrailAid.Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace TrailAid.Data
 {
     public class User
     {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
     }
 }
--- TrailAid.Models/User/UserCreate.cs
+++ TrailAid.Models/User/UserCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace TrailAid.Models.User
 {
     public class UserCreate
     {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
     }
 }
