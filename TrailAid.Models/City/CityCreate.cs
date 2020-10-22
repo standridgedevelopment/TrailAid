502ff67738f74d5e0a28c1da7e1155fb93d2225c
standridgedevelopment <standridgedevelopment@gmail.com>
2020-10-22 16:33:13 -0400

    City Service Complete

--- TrailAid.Models/City/CityCreate.cs
+++ TrailAid.Models/City/CityCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace TrailAid.Models.City
 {
     public class CityCreate
     {
-        // City Addition
+        [Required]
+        public string Name { get; set; }
     }
 }
--- TrailAid.Services/CityService.cs
+++ TrailAid.Services/CityService.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrailAid.Data;
+using TrailAid.Models.City;
 
 namespace TrailAid.Services
 {
     public class CityService
     {
+        private readonly Guid _userId;
+
+        public CityService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateCities(CityCreate model)
+        {
+            var entity = new City()
+            {
+                Name = model.Name
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Cities.Add(entity);
+                //var testing = ctx.SaveChanges();
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CityListItem> GetCities()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Cities.Select
+                    (e => new CityListItem
+                    {
+                        Name = e.Name
+                    }
+                    );
+                return query.ToArray();
+            }
+
+        }
+        public CityDetail GetCityByID(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Cities.Single(e => e.ID == id);
+                return new CityDetail
+                {
+                    Name = entity.Name
+                };
+            }
+        }
+        public bool UpdateCity(CityEdit model, int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Cities.Single(e => e.ID == id);
+
+                entity.Name = model.Name;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteCity(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Cities.Single(e => e.ID == id);
+
+                ctx.Cities.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
