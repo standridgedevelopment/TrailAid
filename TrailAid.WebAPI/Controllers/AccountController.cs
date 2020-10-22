d95fc393791cc71cdad26841cf41c374f732348b
Austin <ausjwill@gmail.com>
2020-10-22 13:46:02 -0400

    Using

--- TrailAid.WebAPI/Controllers/AccountController.cs
+++ TrailAid.WebAPI/Controllers/AccountController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
+using TrailAid.Data;
 using TrailAid.WebAPI.Models;
 using TrailAid.WebAPI.Providers;
 using TrailAid.WebAPI.Results;
