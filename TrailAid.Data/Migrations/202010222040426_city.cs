1b04229ca81a9597fe4c3423915987179f15a072
dfdevaney <dfdevaney@gmail.com>
2020-10-22 15:46:16 -0500

    dev update

---
+++ TrailAid.Data/Migrations/202010222040426_city.cs
@@ -0,0 +1,26 @@
+namespace TrailAid.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class city : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.City",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.City");
+        }
+    }
+}
