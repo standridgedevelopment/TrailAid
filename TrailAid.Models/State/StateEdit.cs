7961a57a1a40f6907f42293d8c17a6d855f06f03
dfdevaney <dfdevaney@gmail.com>
2020-11-02 13:02:51 -0600

    State Start

---
+++ TrailAid.Models/State/StateEdit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailAid.Models.State
+{
+    class StateEdit
+    {
+    }
+}
